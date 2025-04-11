e3de2242aec2021317dfef082ec6e950306685c7
Mark Malstrom <mark@malstrom.me>
2025-04-11 02:57:39 -0500

    Update database bootstrap script

--- src/scripts/db.bootstrap.ts
+++ src/scripts/db.bootstrap.ts
@@ -1,5 +1,8 @@
 import "jsr:@std/dotenv/load";
 import { assert } from "@std/assert";
+import { DatabaseSync } from "node:sqlite";
+import { createDrizzle } from "~/database/node-sqlite.ts";
+import { migrate } from "drizzle-orm/sqlite-proxy/migrator";
 
 const DATABASE_URL = Deno.env.get("DATABASE_URL");
 assert(DATABASE_URL, "Must define DATABASE_URL in .env file");
@@ -13,6 +16,10 @@ try {
 
 	// Create necessary tables using drizzle-kit push
 	// await $`deno run -A npm:drizzle-kit push`;
+	const client = new DatabaseSync(DATABASE_URL);
+	const db = createDrizzle(client);
+	// FIXME: Manually migrate because drizzle-kit isn't working with Deno
+	migrate(db, async () => {}, { migrationsFolder: "./drizzle" });
 
 	// console.log("Database bootstrapped successfully");
 } catch (error) {
